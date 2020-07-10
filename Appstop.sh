9d2853e7685a77df5e19249039e7977bc615deee
23198 <31165700+23198@users.noreply.github.com>
2020-07-10 14:14:45 +0530

    Delete Appstop.sh

--- Appstop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#sudo unlink /var/www/html/smaple-wordpress/wp-content/uploads
-sudo systemctl stop apache2
-if [ -d /var/www/html/sample-wordpress ];then 
-      sudo rm -rf /tmp/sample-wordpress*
-      sudo mv /var/www/html/sample-wordpress /tmp/sample-wordpress"$(date +"%d-%m-%Y")".back
-fi
