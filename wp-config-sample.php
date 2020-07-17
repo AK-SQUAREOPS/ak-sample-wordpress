fac919047ef53bdada28e3a536b1dd2bf0fed979
23198 <31165700+23198@users.noreply.github.com>
2020-07-17 12:39:03 +0530

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -58,8 +58,6 @@ define('FORCE_SSL_ADMIN', true);
 
 if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
    $_SERVER['HTTPS']='on';
-define('WP_HOME','https://newdnscdn.squareops.xyz/');
-define('WP_SITEURL','https://newdnscdn.squareops.xyz/');
 
 /**#@-*/
 
